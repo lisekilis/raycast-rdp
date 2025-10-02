30212f504d4416b5f637b808bdd4881677b2c794
lisekilis <101191226+lisekilis@users.noreply.github.com>
2025-10-02 11:14:23 +0200

    Rename 'address' to 'server' in RDP command arguments for clarity

--- src/rdp.ts
+++ src/rdp.ts
@@ -2,20 +2,20 @@ import { showHUD, LaunchProps } from "@raycast/api";
 import { exec } from "child_process";
 
 export default async function main(props: LaunchProps) {
-  const address = props.arguments.address;
-  if (!address) {
-    await showHUD("No address provided");
+  const server = props.arguments.server;
+  if (!server) {
+    await showHUD("No server address provided");
     return;
   }
 
-  await showHUD(`Starting RDP session to ${address}`);
+  await showHUD(`Starting RDP session to ${server}`);
 
   //check os and run appropriate command
   const os = process.platform;
   switch (os) {
     case "win32":
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      exec(`mstsc /v:${address}`, async (error: any, stdout: any, stderr: any) => {
+      exec(`mstsc /v:${server}`, async (error: any, stdout: any, stderr: any) => {
         if (error) {
           console.error(`exec error: ${error}`);
           await showHUD(`Error starting RDP session: ${error.message}`);
@@ -27,7 +27,7 @@ export default async function main(props: LaunchProps) {
       break;
     case "darwin":
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      exec(`open rdp://full%20address=s:${address}`, async (error: any, stdout: any, stderr: any) => {
+      exec(`open rdp://full%20address=s:${server}`, async (error: any, stdout: any, stderr: any) => {
         if (error) {
           console.error(`exec error: ${error}`);
           await showHUD(`Error starting RDP session: ${error.message}`);
